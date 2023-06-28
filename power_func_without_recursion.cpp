df23a427fcf3125641ed4cd221ea27877eaf2e9a
Sopheak Saing <saingsopheak02@gmail.com>
2023-06-28 23:11:48 +0700

    add power func without recursion

---
+++ power_func_without_recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int power(double base, int exponent)
+{
+    int result = 1.0;
+
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+int main()
+{
+    double base;
+    int exponent;
+    std::cout << "enter the base:";
+    std::cin >> base;
+
+    std::cout << "Enter the exponent:";
+    std::cin >> exponent;
+
+    double result = power(base, exponent);
+    std::cout << base << "raised to the power of " << exponent << "is: " << result << std::endl;
+}
