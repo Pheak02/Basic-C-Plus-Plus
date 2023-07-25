67a8df46ab9e5eabb010de5fbd16e41bd8623c92
Sopheak Saing <saingsopheak02@gmail.com>
2023-07-25 23:27:37 +0700

    time complexity n(1)

---
+++ time-complexity/saingsopheak-lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void timeConstant()
+{
+    string universeName = "Parallel Universe";
+    cout << "Hello from external universe, to " << universeName << endl;
+}
+
+int main()
+{
+    timeConstant();
+}
