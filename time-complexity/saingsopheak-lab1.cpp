094bcb098ffe1732cbef40dfd4ac69041ecbe125
Sopheak Saing <saingsopheak02@gmail.com>
2023-07-26 00:29:04 +0700

    time complexity  Quadratic time — O(n2 )

--- time-complexity/saingsopheak-lab1.cpp
+++ time-complexity/saingsopheak-lab1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-void timeConstant()
+void constantFun()
 {
     string universeName = "Parallel Universe";
     cout << "Hello from external universe, to " << universeName << endl;
 }
-void logarithmicFunction(int n)
+void logarithmicFun(int n)
 {
     cout << "Logarithmic time complexity — O(log n) : \t";
     for (int i = 1; i < n; i *= 2)
@@ -13,11 +13,34 @@ void logarithmicFunction(int n)
         cout << i << ",\t";
     }
 }
+void LinearFun(int n)
+{
+    cout << "\nLinear time complexity — O(n) : \t";
+    // because iterate n time(s)
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << ", ";
+    }
+}
+void quadraticFun(int n)
+{
+    cout << "\nLinear time complexity — O(n^2) : \n";
+
+    for (int i = 0; i < n; i++)
+    { // O(n)
+        for (int j = 0; j < n; j++)
+        { // O(n)
+            cout << "i: " << i << ", j: " << j << endl;
+        }
+    }
+}
 
 int main()
 {
     int n = 3;
-    timeConstant();
-    logarithmicFunction(n);
+    constantFun();
+    logarithmicFun(n);
+    LinearFun(n);
+    quadraticFun(n);
     return 0;
 }
