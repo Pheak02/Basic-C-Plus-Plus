d1f530bd46701f03ecde6957d405590fd5276761
Sopheak Saing <saingsopheak02@gmail.com>
2023-07-26 00:14:16 +0700

    time complexity O(log n)

--- time-complexity/saingsopheak-lab1.cpp
+++ time-complexity/saingsopheak-lab1.cpp
@@ -5,8 +5,19 @@ void timeConstant()
     string universeName = "Parallel Universe";
     cout << "Hello from external universe, to " << universeName << endl;
 }
+void logarithmicFunction(int n)
+{
+    cout << "Logarithmic time complexity — O(log n) : \t";
+    for (int i = 1; i < n; i *= 2)
+    {
+        cout << i << ",\t";
+    }
+}
 
 int main()
 {
+    int n = 3;
     timeConstant();
+    logarithmicFunction(n);
+    return 0;
 }
