773e1ac58d190bf64810251268847942fa2a8f05
Sopheak Saing <saingsopheak02@gmail.com>
2023-08-06 17:32:32 +0700

    simple if else statement

---
+++ introduction/ifElse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a integer: ";
+    cin >> num;
+    if (num > 0)
+    {
+        cout << "This number is positive";
+    }
+    else
+    {
+        cout << "This number is negative";
+    };
+}
