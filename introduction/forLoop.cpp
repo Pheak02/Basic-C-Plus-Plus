9755fa51a6187eb13f4b40fa804b5527b39d6305
Sopheak Saing <saingsopheak02@gmail.com>
2023-08-06 18:31:18 +0700

    sum number using for loop

--- introduction/forLoop.cpp
+++ introduction/forLoop.cpp
@@ -2,9 +2,29 @@
 using namespace std;
 int main()
 {
-    int i = 0;
-    for (i; i < 5; ++i)
+    //====FIRST WAY======
+    // int i = 0;
+    // for (i; i < 5; ++i)
+    // {
+    //     cout << i << " ";
+    // }
+    //====SECOND WAY======
+
+    // for (int i; i < 5; ++i)
+    // {
+    //     cout << i << " ";
+    // }
+
+    /* =======SUM number==== */
+    int userInput;
+    int sum = 0;
+    // cout << "Enter a number: ";
+    // cin >> userInput;
+    for (int i = 0; i <= 5; i++)
     {
-        cout << i << " ";
+        cout << "Enter a number: ";
+        cin >> userInput;
+        sum += userInput;
     }
+    cout << "The sum of your number is:" << sum;
 }
